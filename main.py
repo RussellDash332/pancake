22dea48b353c518b339a052f7dbe1ae254e5b754
Russell Saerang <63991775+RussellDash332@users.noreply.github.com>
2023-04-02 06:19:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
-import os, platform, requests, time
+import os, time, sys
 from bs4 import BeautifulSoup
-from zipfile import ZipFile
-from urllib.request import urlretrieve
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 from pancake import Pancake, DeluxePancake
 
-HTML_FN = 'run.html'
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
-version = requests.get('https://chromedriver.storage.googleapis.com/LATEST_RELEASE').text
-curr_os = platform.platform()
-
-def download_chromedriver():
-    try: os.remove('chromedriver.zip')
-    except: pass
-    print('Downloading "chromedriver.exe"...')
-    url = f'https://chromedriver.storage.googleapis.com/{version}/chromedriver_win32.zip'
-    urlretrieve(url, 'chromedriver.zip')
-    with ZipFile('chromedriver.zip') as chromezip:
-        chromezip.extractall()
-    os.remove('chromedriver.zip')
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
 
 def start_browser(mode=None):
     if mode == None:
-        mode = int(input('Daily Waffle or Deluxe Waffle? (1 or 2)\n'))
-        assert mode in [1, 2], 'Invalid mode selected'
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    browser = webdriver.Chrome(options=options)
+        mode = int(sys.argv[1])
+    browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
     browser.maximize_window()
     browser.set_page_load_timeout(30)
     try:
@@ -72,7 +71,6 @@ def start_browser(mode=None):
         return start_browser(mode)
     html_doc = browser.page_source
     browser.quit()
-    #open(HTML_FN, 'wb+').write(html_doc.encode())
     return BeautifulSoup(html_doc, 'html.parser')
 
 def parse(soup):
@@ -99,23 +97,11 @@ def parse(soup):
     else:           print(f'Ignoring invalid size of {size}...')
 
 if __name__ == '__main__':
-    if curr_os.startswith('Windows'):
-        try:
-            soup = start_browser()
-        except Exception as e:
-            print('Issue found:', e)
-            download_chromedriver()
-            soup = start_browser()
-        print('Done! Parsing source page now...\n')
-        parse(soup)
-        print()
-    while True:
-        try:
-            print(input(f'Paste the whole page source at {HTML_FN}, and press Enter when you\'re done:\n'))
-            html_doc = ''.join(map(bytes.decode, open(HTML_FN, 'rb').readlines()))
-            parse(BeautifulSoup(html_doc, 'html.parser'))
-            print()
-        except KeyboardInterrupt: break
-        except Exception as e:
-            print('HTML page source cannot be parsed :( Skipping!')
-            print('Issue:', e)
+    try:
+        soup = start_browser()
+    except Exception as e:
+        print('Issue found:', e)
+        download_chromedriver()
+        soup = start_browser()
+    print('Done! Parsing source page now...\n')
+    parse(soup)
